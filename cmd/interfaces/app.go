59c21563da4f7c3a67a610c91d849485f111bb73
Ad3bay0c <aclintonadebayo@gmail.com>
2021-10-05 21:06:01 +0100

    Interfaces Added

---
+++ cmd/interfaces/app.go
@@ -0,0 +1,22 @@
+package interfaces
+
+import "github.com/Ad3bay0c/BlogCMS/cmd/models"
+
+type Commentable interface {
+
+}
+
+type User interface {
+	GetUserByEmail(email string) (bool, models.User)
+	SaveUser(user *models.User) (string, error)
+	GetUserById(id string) (models.User, error)
+}
+
+type Blogger interface {
+	SavePost(post models.Post) error
+	GetPostsByUserId(userId string) ([]models.Post, error)
+	GetAllPost(userId string) ([]models.Post, error)
+	UpdatePost(post models.Post) error
+	ViewBlogPostById(postId string) (models.Post, error)
+	DeletePostById(postId string, userId string) error
+}
