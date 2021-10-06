c3fb7b3631560585d004f333e54c59fcfe2c8a3a
Ad3bay0c <aclintonadebayo@gmail.com>
2021-10-06 22:09:47 +0100

    count comments handler added

--- cmd/web/commentController.go
+++ cmd/web/commentController.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+	"github.com/Ad3bay0c/BlogCMS/cmd/models"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func (app *Application) CreateComment(c *gin.Context){
+	param, _ := c.Get("userId")
+	userId := param.(string)
+
+	id := c.Param("id")
+
+	if strings.TrimSpace(c.PostForm("comment")) == "" {
+		m["Message"] = "Comment is Required"
+		m["Color"] = "danger"
+		c.Redirect(302, fmt.Sprintf("/blog/%s/", id))
+		return
+	}
+	comment := models.Comment{
+		ID:      uuid.New().String(),
+		Comment: strings.TrimSpace(c.PostForm("comment")),
+		PostId:  id,
+		CreatedAt: time.Now().Unix(),
+		UpdatedAt: time.Now().Unix(),
+		UserId: userId,
+	}
+
+	err := app.commentModel.CreateComment(comment)
+
+	if err != nil {
+		app.errorLog.Printf(err.Error())
+		m["Message"] = http.StatusText(http.StatusInternalServerError)
+		m["Color"] = "danger"
+		c.Redirect(302, fmt.Sprintf("/blog/%s/", id))
+
+		return
+	}
+
+	m["Message"] = "Comment Created Successfully"
+	m["Color"] = "success"
+
+	c.Redirect(http.StatusFound, fmt.Sprintf("/blog/%s/", id))
+}
