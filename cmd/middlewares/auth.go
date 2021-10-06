d6b121a2798413215ac3284bbf7865ff3f746c79
Ad3bay0c <aclintonadebayo@gmail.com>
2021-10-06 15:57:26 +0100

    Middlewares Modified

--- cmd/middlewares/auth.go
+++ cmd/middlewares/auth.go
@@ -13,6 +13,19 @@ func CheckLogin() gin.HandlerFunc {
 			return
 		}
 		c.Set("userId", session)
+		//c.Set("is_Logged_in", true)
 		c.Next()
+	}
+}
+
+func CheckNotLogin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		_, err := c.Cookie("session")
+		if err != nil {
+			c.Next()
+			return
+		}
+		c.Redirect(http.StatusFound, "/blog/")
+		return
 	}
 }
